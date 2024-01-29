52ab5af8a64b3b4228553ad7d1780efcf7e31e21
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2024-01-29 21:26:55 +0530

    added metrimonial

--- momo_website/src/sections/testimonial.jsx
+++ momo_website/src/sections/testimonial.jsx
@@ -4,26 +4,48 @@ import Heading1 from "../components/heading1";
 
 const Testimonial_section = ({ clss = "", title = "", background = "" }) => {
 
-    let chatney = [
-        { image: "/image/paneer_momos-removebg-preview.png", info: "Our delivery facility makes every momo lover happy", user: "Rohan", rating: "4" },
-        { image: "/image/veg_momos-removebg-preview.png", info: "Our delivery facility makes every momo lover happy", user: "Roshan", rating: "5" },
-        { image: "/image/chicken_momos-removebg-preview.png", info: "Oue delivery facility makes every momo lover happy", user: "Rahul", rating: "3" }
+    let ratingsOnPlates = [
+        { image: "/image/paneer_momos-removebg-preview.png",
+        type : "paneer", 
+        ratings: [
+            {info: "Our delivery facility makes every momo lover happy", user: "Rahul", rating: "4"},
+            {info: "Our delivery facility makes every momo lover happy", user: "Rohan", rating: "3"},
+            {info: "Our delivery facility makes every momo lover happy", user: "Rohit", rating: "5"},
+        ] 
+    },
+
+        { image: "/image/veg_momos-removebg-preview.png", rating: "4", 
+        type : "veg", 
+        ratings : [
+        {info: "Our delivery facility makes every momo lover happy", user: "Rohan", rating: "3"},
+        {info: "Our delivery facility makes every momo lover happy", user: "Rohit", rating: "5"},
+    ] 
+},
+        { image: "/image/chicken_momos-removebg-preview.png", 
+        type : "chicken", 
+        ratings : [
+            {info: "Our delivery facility makes every momo lover happy", user: "Rohan", rating: "3"},
+            {info: "Our delivery facility makes every momo lover happy", user: "Rohit", rating: "5"},
+        ]  }
     ]
 
     return (
         <div className="container testimonial_container" style={{ backgroundColor: `${background}` }}>
             <Heading1 title="Customers" clss="heading1 heading" background={background} />
 
-            {chatney.map((v, i) => {
+            {ratingsOnPlates.map((v, i) => {
 
                 return <div key={i} className="card">
                     <div className="section1" style={{ order: `${i % 2 == 0 ? "1" : "2"}` }}>
                         <img src={v.image} alt={v.image} />
-                        <h4 className="title">Paneer</h4>
-                        <div className="box">
+                        <h4 className="title">{v.type}</h4>
+                        <div className="flex">
+                       
+                       {v.ratings.map((val,ind)=>{
+                        return <div key={ind} className="box">
                             <p>
                                 <i class="ri-double-quotes-l"></i>
-                                {v.info}
+                                {val.info}
                                 <i class="ri-double-quotes-r"></i>
                             </p>
                             <span style={{ display: "flex" }}>
@@ -33,11 +55,12 @@ const Testimonial_section = ({ clss = "", title = "", background = "" }) => {
                                 <i class="ri-star-line"></i>
                                 <i class="ri-star-line"></i>
                             </span>
-                            <span>{v.user}</span>
+                            <span>{val.user}</span>
+                        </div>
+                    })}
                         </div>
                         <div className="scroll_btns">
                             <button><i class="ri-arrow-left-s-line"></i></button>
-
                             <button><i class="ri-arrow-right-s-line"></i></button>
                         </div>
                     </div>
